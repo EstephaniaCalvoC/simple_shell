f3ab804d6ad2d455892d18ea0f05eb8b58a8baf4
David Alzate <illker@hotmail.com>
2020-11-23 02:24:19 +0000

    add _strcmp function :point_right: :point_left:

--- str_f1.c
+++ str_f1.c
@@ -53,3 +53,23 @@ char *_strcpy(char *dest, char *src)
 	dest[i] = '\0';
 	return (dest);
 }
+
+/**
+ * _strcmp - Compare two strings.
+ * @s1: string 1.
+ * @s2: string 2.
+ * Return: 0 if the string are the same.
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	for (; (*s1 != '0' && *s2 != '\0') && *s1 == *s2;)
+	{
+		s1++;
+		s2++;
+	}
+	if (*s1 == *s2)
+		return (0);
+	else
+		return (*s1 - *s2);
+}
