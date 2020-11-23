a86fda646823dd2b32c0239a6b9a45f8203a1ac2
David Alzate <illker@hotmail.com>
2020-11-23 00:34:45 +0000

    add _strcpy function :octopus:

--- str_f1.c
+++ str_f1.c
@@ -36,3 +36,20 @@ char *_strcat(char *dest, char *src)
 	dest[index] = '\0';
 	return (dest);
 }
+
+/**
+ * _strcpy - copy the string in src to dest with null at end,
+ * @dest: destiny char
+ * @src: source char
+ * Return: copy char in destiny
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++)
+		dest[i] = src[i];
+	dest[i] = '\0';
+	return (dest);
+}
